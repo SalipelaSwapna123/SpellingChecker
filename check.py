1eb47fbd60cf2a931de61822bd3f7c82da75040d
Salipela Swapna <130962712+SalipelaSwapna123@users.noreply.github.com>
2023-08-22 12:34:34 +0530

    check.py

--- check.py
+++ check.py
@@ -1,27 +1,30 @@
-#Importing Required Modules:
 import tkinter
-from tkinter import •
+from tkinter import *
 from textblob import TextBlob
-#Creating the GUI Window:
+
 root=Tk()
-root.title("Spelling Checker")
+root.title("Spelling Checker") 
 root.geometry("700x480") 
 root.config(background="#dae6f6")
-#Defining the Spelling Check Function:
-def check_spelling():
-    word = enter_text.get()
-    corrected_text = str(TextBlob(word).correct())
-    spell.config(text=corrected_text)
-#Creating GUI Elements:
-heading = Label(root, text="Spelling Checker", font=("Trebuchet MS", 30, "bold"), bg="#dae6f6", fg="#364971")
-heading.pack(pady=(50, 0))
 
-enter_text = Entry(root, justify="center", width=30, font=("poppins", 25), bg="white", border=2)
+def check_spelling():
+    word=enter_text.get()
+    a=TextBlob (word)
+    right=str(a.correct())
+
+    cs=Label(root, text="Correct text is :", font=("poppins", 20), bg="#dae6f6", fg="#364971")
+    cs.place(x=100,y=250)
+    spell.config(text=right)
+
+heading = Label(root, text="Spelling Checker", font=("Trebuchet MS", 30, "bold"), bg="#dae6f6", fg="#364971")
+heading.pack(pady=(50,0))
+
+enter_text=Entry(root, justify="center", width=30, font=("poppins", 25), bg="white", border=2)
 enter_text.pack(pady=10)
 enter_text.focus()
 
-button = Button(root, text="Check", font=("arial", 20, "bold"), fg="white", bg="red", command=check_spelling)
+button=Button(root, text="Check", font=("arial", 20, "bold"), fg="white", bg="red", command=check_spelling) 
 button.pack()
 
-spell = Label(root, font=("poppins", 20), bg="#dae6f6", fg="#364971")
-spell.place(x=350, y=250)
+spell=Label(root, font=("poppins", 20), bg="#dae6f6", fg="#364971")
+spell.place(x=350,y=250)
